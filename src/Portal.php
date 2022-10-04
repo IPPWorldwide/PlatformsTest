93273e473f5dde753070d3de0a518cbd0d41f5f0
Mathias Gajhede <mag@venturecapitals.dk>
2022-10-04 11:51:12 +0200

    Update Portal.php

--- src/Portal.php
+++ src/Portal.php
@@ -5,15 +5,13 @@ class Portal {
     private $ENV = null;
     public function curl ($url,$post_data) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://merchantadmin/" . $url);
+        curl_setopt($ch, CURLOPT_URL, $_ENV["MERCHANT_URL"] . $url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
         $server_output = curl_exec($ch);
         curl_close($ch);
-        var_dump($_ENV["MERCHANT_URL"] . $url);
-        var_dump($server_output);
         return $server_output;
     }
 }
