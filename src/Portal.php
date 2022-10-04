a26f91f4d166c910d397400871699347e5c4ac35
Mathias Gajhede <mag@venturecapitals.dk>
2022-10-04 11:22:01 +0200

    PortalTest

---
+++ src/Portal.php
@@ -0,0 +1,17 @@
+<?php
+namespace App;
+
+class Portal {
+    private $ENV = null;
+    public function curl ($url,$post_data) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($server_output);
+    }
+}
---
+++ test/PortalTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Test;
+use App\Portal;
+use App\Partner;
+
+if($_ENV["TEST_PORTAL"] === "1") {
+    class PortalTest extends \PHPUnit\Framework\TestCase {
+        public function testSetup() {
+            $partner    = new Partner();
+            $portal     = new Portal();
+            $login = $partner->login($_ENV["PARTNER_USERNAME"], $_ENV["PARTNER_PASSWORD"])->content;
+            $data = $partner->data($login->user_id,$login->session_id)->content;
+            self::assertObjectHasAttribute(
+                "key1",
+                $data->security
+            );
+            self::assertObjectHasAttribute(
+                "key2",
+                $data->security
+            );
+            $portal->curl("/setup/", [
+                "portal_title" => "TestingPortal",
+                "administrator_email" => $_ENV["PARTNER_USERNAME"],
+                "currency"  => "978",
+                "portal_url"    => $_ENV["MERCHANT_URL"],
+                "partner_id"    => $data->id,
+                "partner_key1"  => $data->security->key1,
+                "partner_key2"  => $data->security->key2,
+            ]);
+            $portal_html = $portal->curl("/", []);
+            $this->assertContains("TestingPortal", $portal_html );
+
+        }
+        public function testPartnerData() {
+            $partner = new Partner();
+            $login = $partner->login($_ENV["PARTNER_USERNAME"], $_ENV["PARTNER_PASSWORD"])->content;
+            $data = $partner->data($login->user_id,$login->session_id)->content;
+            self::assertObjectHasAttribute(
+                "name",
+                $data
+            );
+        }
+    }
+}
